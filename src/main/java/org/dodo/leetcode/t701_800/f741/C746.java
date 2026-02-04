eb3ae97ac216f35cfda998a195101593f1f760d8
dodo2390183798 <2390183798@qq.com>
2026-02-04 16:14:24 +0800

    添加 746 的 使用最小花费爬楼梯 4种解法

--- src/main/java/org/dodo/leetcode/t701_800/f741/C746.java
+++ src/main/java/org/dodo/leetcode/t701_800/f741/C746.java
@@ -68,9 +68,15 @@ public class C746 implements ISolve {
 //		int[] cost = new int[]{1,100,1,1,1,100,1,1,100,1};	// 答案 6
 		int num = solve01(cost);
 		int num02 = solve02(cost);
+		int num03 = solve03(cost);
+		int num04 = solve04(cost);
 		Lg.info("使用最小花费爬楼梯1 is " + num);
 		Lg.info("  =  ");
 		Lg.info("使用最小花费爬楼梯2 is " + num02);
+		Lg.info("  =  ");
+		Lg.info("使用最小花费爬楼梯3 is " + num03);
+		Lg.info("  =  ");
+		Lg.info("使用最小花费爬楼梯4 is " + num04);
 	}
 
 	public void minCostClimbingStairs(int[] cost) {
@@ -108,6 +114,27 @@ public class C746 implements ISolve {
 			mini[i] = Math.min(mini[i-1] + cost[i], mini[i-2] + cost[i-1]);
 		}
 		return mini[length-1];
+	}
+
+
+	public int solve03(int[] cost){
+		int sumEnd0 = 0;
+		int sumEnd1 = 0;
+		for(int i=2; i<=cost.length; i++){
+			int next = Math.min(sumEnd1 + cost[i-1], sumEnd0 + cost[i-2]);
+			sumEnd0 = sumEnd1;
+			sumEnd1 = next;
+		}
+		return sumEnd1;
+	}
+
+
+	public int solve04(int[] cost){
+		int n = cost.length;
+		for(int i=2; i<n; i++){
+			cost[i] += Math.min(cost[i-1], cost[i-2]);
+		}
+		return Math.min(cost[n-1], cost[n-2]);
 	}
 
 
