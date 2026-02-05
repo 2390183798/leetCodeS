b06b5fe253f0072fb2a91a98b0b33b7e81f63de3
dodo2390183798 <2390183798@qq.com>
2026-02-05 09:50:29 +0800

    修改结构，简单提交代码

--- src/main/java/org/dodo/LeetCodeApplication.java
+++ src/main/java/org/dodo/LeetCodeApplication.java
@@ -1,21 +1,11 @@
 package org.dodo;
 
-import org.dodo.utils.DoScanner;
+import org.dodo.type.dtgh.TypeDtgh;
 
 public class LeetCodeApplication {
 
 	public static void main(String[] args) {
-		doDtgh();
-	}
-
-	/**
-	 * 动态规划： utils/type/动态规划
-	 */
-	static void doDtgh(){
-//		DoScanner.doSth(70);	// 爬楼梯
-//		DoScanner.doSth(509);		// 斐波那契数列
-//		DoScanner.doSth(1137);		// 第N个 泰波那契数
-		DoScanner.doSth(746);		// 使用最小花费爬楼梯
+		TypeDtgh.doSth();	// 动态规划
 	}
 
 }
--- src/main/java/org/dodo/leetcode/t101_200/f191/C198.java
+++ src/main/java/org/dodo/leetcode/t101_200/f191/C198.java
@@ -38,25 +38,74 @@ public class C198 implements ISolve {
 //			1 <= nums.length <= 100
 //			0 <= nums[i] <= 400
 
+//	参考： https://leetcode.cn/problems/house-robber/solutions/138131/dong-tai-gui-hua-jie-ti-si-bu-zou-xiang-jie-cjavap/?envType=study-plan-v2&envId=dynamic-programming
+
+//	思路：
+//	f(k) 最大， 就看。f(k-2) + num[k-1]  和 f(k-1) 谁大
+//
+//	当 n=0，  f(0) = 0
+//	当 n=1，  f(1) = num[0]
+//	当 n=2，  f(2) = max( f(0) + num[1],  f(1) )
+//	当 n=3，  f(3) = max( f(1) + num[2],  f(2) )
+//
+
+
 	String TAG = "打家劫舍";
 
 	@Override
 	public void doSth() {
 //		int[] data = new int[]{1,2,3,1};	// 4
 		int[] data = new int[]{2,7,9,3,1};	// 12
-		int num = rob(data);
-		int num02 = rob02(data);
-		Lg.info(TAG + "1 is " + num);
+		Lg.info(TAG + "1 is " + rob(data));
 		Lg.info("  =  ");
-		Lg.info(TAG + "2 is " + num02);
+		Lg.info(TAG + "2 is " + rob02(data));
+//		Lg.info("  =  ");
+//		Lg.info(TAG + "3 is " + rob03(data));
 	}
 
 	public int rob(int[] nums) {
-		return 0;
+		int n = nums.length;
+		if(n == 0){
+			return 0;
+		}
+
+		int[] dp = new int[n+1];
+		dp[0] = 0;
+		dp[1] = nums[0];
+		for(int i=2; i<=n; i++){
+			dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i-1]);
+		}
+		return dp[n];
 	}
 
+
 	public int rob02(int[] nums) {
-		return 0;
+		int n = nums.length;
+		if(n == 0){
+			return 0;
+		}
+
+		int pre = 0;
+		int cur = 0;
+		for(int i=2; i<n; i++){
+			int next = Math.max(cur, pre+nums[i]);
+			cur = pre;
+			pre = next;
+		}
+		return cur;
+	}
+
+
+	public int rob03(int[] nums) {
+		int n = nums.length;
+		if(n == 0){
+			return 0;
+		}
+
+		for(int i=2; i<n; i++){
+			nums[i] += Math.max(nums[i-1], nums[i-2]);
+		}
+		return Math.max(nums[n-1], nums[n-2]);
 	}
 
 }
