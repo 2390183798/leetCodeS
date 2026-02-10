97fd4c8fda9a5d7b000587324085df0b425a6ac7
dodo2390183798 <2390183798@qq.com>
2026-02-10 10:37:07 +0800

    740. 删除并获得点数

--- src/main/java/org/dodo/type/dtgh/TypeDtgh.java
+++ src/main/java/org/dodo/type/dtgh/TypeDtgh.java
@@ -4,12 +4,27 @@ import org.dodo.utils.DoScanner;
 
 public class TypeDtgh {
     public static void doSth(){
+//        do01Fib();
+        do02Matrix();
+    }
+
+    /**
+     * 斐波那契数列 相关
+     */
+    static void do01Fib(){
 //		DoScanner.doSth(70);	// 爬楼梯
 //		DoScanner.doSth(509);		// 斐波那契数列
 //		DoScanner.doSth(1137);		// 第N个 泰波那契数
 //		DoScanner.doSth(746);		// 使用最小花费爬楼梯
 //        DoScanner.doSth(198);		// 打家劫舍
         DoScanner.doSth(740);		// 删除并获得点数 ？？？
-//        DoScanner.doSth(62);		// 删除并获得点数 ？？？
     }
+
+    /**
+     * 斐波那契数列 相关
+     */
+    static void do02Matrix(){
+        DoScanner.doSth(62);		//  不同路径
+    }
+
 }
